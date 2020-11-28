b5357ccb5225f119680f23657a847abf0613992f
Muneeb Ghani <muneebg2@gmail.com>
2020-11-28 18:12:14 +0500

    0fcommit

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -3,9 +3,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME || dsbx0t4g3,
-  api_key: process.env.CLOUDINARY_API_KEY || 653991295757619,
-  api_secret: process.env.CLOUDINARY_API_SECRET || WYtGHRRw3hMuiqxxeOcACKOFte8,
+  cloud_name: dsbx0t4g3,
+  api_key: 653991295757619,
+  api_secret: WYtGHRRw3hMuiqxxeOcACKOFte8,
 });
 
 module.exports = cloudinary;
